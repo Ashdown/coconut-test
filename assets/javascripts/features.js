a5670e73cf9b9721dea7272114e6d06d72afb314
Rory Devane <rory.devane@gmail.com>
2017-02-19 21:15:36 +0000

    integrated controls

--- assets/javascripts/features.js
+++ assets/javascripts/features.js
@@ -4,13 +4,29 @@
 
     console.log('features.js');
 
-    $('.features-list').slick({
+    var $featuresList = $('.features-list'),
+        initialSlide = Math.floor($('.feature-item').size() / 2);
+
+    $('.nav-link').on('click', function(event) {
+        event.preventDefault();
+        $featuresList.slick('slickGoTo', $('.nav-link').index(this));
+    });
+
+    $featuresList.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        $('.nav-link').removeClass('selected');
+        $('.nav-link').eq(nextSlide).addClass('selected');
+    });
+
+    $featuresList.slick({
         mobileFirst: true,
         arrows: false,
         infinite: false,
         dots: false,
-        slide: '.feature-item'
+        slide: '.feature-item',
+        initialSlide: initialSlide
     });
+
+    $('.nav-link').eq(initialSlide).addClass('selected');
 
 
 })();
