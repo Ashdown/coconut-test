fd854f876d58588f37c586a9e4ba125905af9da5
Rory Devane <rory.devane@gmail.com>
2017-02-19 21:33:51 +0000

    pyramid fade in effect

--- assets/javascripts/features.js
+++ assets/javascripts/features.js
@@ -1,21 +1,63 @@
-(function(){
+(function(window) {
+    var helpers = {
+        getPageScrollTop: function () {
+            var doc = document.documentElement;
+            return (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+        },
+
+        getPageScrollBottom: function () {
+            return this.getPageScrollTop() + window.innerHeight;
+        }
+    };
+    window.helpers = helpers;
+})(window);
+
+(function(helpers){
 
     'use strict';
 
-    console.log('features.js');
-
     var $featuresList = $('.features-list'),
-        initialSlide = Math.floor($('.feature-item').size() / 2);
+        initialSlide = Math.floor($('.feature-item').size() / 2),
+        $navLinks = $('.nav-link');
 
-    $('.nav-link').on('click', function(event) {
+    function checkForFadeIn() {
+
+        var pageScrollBottom = helpers.getPageScrollBottom(),
+            bottomPositionOfList =  $featuresList.offset().top;
+
+        function fadeInSlides(offset) {
+            window.setTimeout(function() {
+                $navLinks.eq(initialSlide-offset).addClass('visible');
+                $navLinks.eq(initialSlide+offset).addClass('visible');
+                if((++offset * 2) < $navLinks.size()) {
+                    fadeInSlides(offset);
+                }
+            }, 100);
+        }
+
+        if( pageScrollBottom >= bottomPositionOfList) {
+
+            $navLinks.eq(initialSlide).addClass('visible');
+
+            fadeInSlides(1);
+
+        }
+
+    }
+
+    $navLinks.on('click', function(event) {
         event.preventDefault();
         $featuresList.slick('slickGoTo', $('.nav-link').index(this));
     });
 
     $featuresList.on('beforeChange', function(event, slick, currentSlide, nextSlide){
-        $('.nav-link').removeClass('selected');
-        $('.nav-link').eq(nextSlide).addClass('selected');
+        $navLinks.removeClass('selected');
+        $navLinks.eq(nextSlide).addClass('selected');
     });
+
+    //detect when this scrolls into view
+
+    window.addEventListener('scroll', checkForFadeIn);
 
     $featuresList.slick({
         mobileFirst: true,
@@ -26,7 +68,8 @@
         initialSlide: initialSlide
     });
 
-    $('.nav-link').eq(initialSlide).addClass('selected');
+    $navLinks.eq(initialSlide).addClass('selected');
 
+    checkForFadeIn();
 
-})();
+})(helpers);
